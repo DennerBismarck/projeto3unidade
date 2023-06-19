e09b240228d885adf0042d29fd6b0c91fe75d03b
DennerBismarck <dennerbismarck@gmail.com>
2023-06-19 12:49:25 -0300

    Versão 2

--- Pybar - Versao2.py
+++ Pybar - Versao2.py
@@ -7,12 +7,21 @@ def menuPrincipal():
     print("\t1 - Funcionários")
     print("\t2 - Produtos")
     print("\t3 - Pedidos")
+    print("\t4 - Informações do projeto")
     print("\t0 - Sair")
     print("\n======================================")
     escolha = input("Digite sua opção: ")
     return escolha
-#Programa inicial principal
 
+#Função para printar as informações do projeto
+def infoProjeto():
+    print("PyBar: Um sistema de gestão para bares e restaurantes!")
+    print("Matéria: DCT1101 - ALGORITMOS E LÓGICA DE PROGRAMAÇÃO")
+    print("Docente: Flavius da Luz e Gorgonio")
+    print("Discente: Denner Bismarck de Lucena França")
+    print("GitHub: https://github.com/DennerBismarck/projeto3unidade")
+
+#Programa inicial principal
 opcao = menuPrincipal()
 while opcao != "0":
     if(opcao == "1"):
@@ -24,6 +33,8 @@ while opcao != "0":
     elif(opcao == "3"):
         print("=============== Menu Pedidos ===============")
         print("Ainda em desenvolvimento...")
+    elif(opcao == "4"):
+        infoProjeto()
     else:
         print("Opção inválida!")
     input("Digite qualquer tecla para continuar")
